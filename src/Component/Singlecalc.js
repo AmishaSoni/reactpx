bce679db33425691382af864a178e0ed09e77e71
AmishaSoni <amishasoni2001@gmail.com>
2023-02-22 22:52:58 +0530

    Again Commit

--- src/Component/Singlecalc.js
+++ src/Component/Singlecalc.js
@@ -3,45 +3,45 @@ import { useState } from 'react';
 
 export const Singlecalc = () => {
 
-
-    // const [num,setNum]=useState(0);
-    
-    const insertNumber = (e,idx) =>{
-      var inputField = document.getElementById("input");
-      inputField.value = inputField.value*10+idx*1
-    }
-
-    const calcNums = () =>{
-      
-    }
+const [num,setNum]=useState('')
+   
+function operation()
+{
+ var output=eval(num);
+ setNum(output);
+}
 
 
   return (
     <div>
-        <input type="text" placeholder="Lets Calculate"  id="input" value={0}></input>
+        <input type="text" placeholder="Lets Calculate"  id="input" value={num}></input>
           <div>
-            <button onClick={(e) => insertNumber(e,"9")}>9</button>
-            <button onClick={(e) => insertNumber(e,"8")}>8</button>
-            <button onClick={(e) => insertNumber(e,"7")}>7</button>
+            <button onClick={()=>setNum(prev=>prev+'9')}>9</button>
+            <button onClick={()=>setNum(prev=>prev+'8')}>8</button>
+            <button onClick={()=>setNum(prev=>prev+'7')}>7</button>
+            <button onClick={()=>setNum(prev=>prev+'6')}>6</button>
           </div>
           <div>
-            <button onClick={(e) => insertNumber(e,"6")}>6</button>
-            <button onClick={(e) => insertNumber(e,"5")}>5</button>
-            <button onClick={(e) => insertNumber(e,"4")}>4</button>
+            
+            <button onClick={()=>setNum(prev=>prev+'5')}>5</button>
+            <button onClick={()=>setNum(prev=>prev+'4')}>4</button>
+            <button onClick={()=>setNum(prev=>prev+'3')}>3</button>
+            <button onClick={()=>setNum(prev=>prev+'2')}>2</button>
           </div>
           <div>
-            <button onClick={(e) => insertNumber(e,"3")}>3</button>
-            <button onClick={(e) => insertNumber(e,"2")}>2</button>
-            <button onClick={(e) => insertNumber(e,"1")}>1</button>
+            
+            <button onClick={()=>setNum(prev=>prev+'1')}>1</button>
+            <button onClick={()=>setNum(prev=>prev+'0')} >0</button>
           </div>
           <div>
-            <button onClick={(e) => calcNums(e)}  >+</button>
-            <button onClick={(e) => calcNums(e)} >-</button>
-            <button onClick={(e) => calcNums(e)} >0</button>
-            <button onClick={(e) => calcNums(e)} >/</button>
-            <button onClick={(e) => calcNums(e)} >*</button>
+            <button onClick={()=>setNum(prev=>prev+'+')}  >+</button>
+            <button onClick={()=>setNum(prev=>prev+'-')} >-</button>
+            <button onClick={()=>setNum(prev=>prev+'/')} >/</button>
+            <button onClick={()=>setNum(prev=>prev+'*')} >*</button>
           </div>
-          {/* <button onClick={eval(num)}>=</button> */}
+
+          <button onClick={operation}>=</button>
+          <button onClick={()=>setNum('')}>Clear</button>
     </div>
     
   
